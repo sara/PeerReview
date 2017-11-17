10e65581c563881e5b1485d1bfc4373a74d1fae8
SaraAnn Stanway <sarastanway@gmail.com>
2017-11-17 16:01:27 -0500

    adding in landing page changes

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+Base = automap_base()
+
+engine = create_engine("mysql://sarastanway:96Ladybug@localhost:8080/PeerReview")
+
+con = engine.connect()
+#Base.prepare(engine, reflect=True)
+
+#Students = Base.classes.students
+#Reviews = Base.classes.reviews
+#Taken = Base.classes.taken
+#Skills = Base.classes.skills
+
+#session = Session(engine)
+
+

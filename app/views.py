b1bc495ab2d291cf097ce651d83d819b1de4df78
SaraAnn Stanway <sarastanway@gmail.com>
2017-11-21 00:11:15 -0500

    renders full prof table, need to update students

--- app/views.py
+++ app/views.py
@@ -35,16 +35,30 @@ def get_students(name):
 
 
 
-@app.route('/profile/<id>', methods = ['GET', 'POST'])
-def get_profile(id):
-  #reviews = session.execute("select * from reviews where partnerid=:val", {'val': id})
-  #Review = namedtuple('Review', result.keys())
-  #reviews = [Review(*r) for r in reviews.fetchall()]
-  #reviews = [review.__dict__ for review in reviews]
-  #return render_template('student.html', reviews=reviews)
-  return 'doop'
+@app.route('/student_profile/<id>', methods = ['GET', 'POST'])
+def get_student_profile(id):
+  reviews = session.execute("select * from reviews where partnerid=:val", {'val': id})
+  review = namedtuple('review', reviews.keys())
+  reviews = [review(*r) for r in reviews.fetchall()]
+  reviews = [review.__dict__ for review in reviews]
+  return render_template('student.html', reviews=reviews)
 
-#select from taken, get only unique classes (i.e. ignore reviewers)
+
+@app.route('/professor_profile/<id>/<course>', methods = ['GET', 'POST'])
+def get_professor_profile(id, course):
+  reviews = session.execute("select * from reviews where prof=:val and classID=:course", {'val': id, 'course': course })
+  review = namedtuple('review', reviews.keys())
+  reviews = [review(*r) for r in reviews.fetchall()]
+  reviews = [review.__dict__ for review in reviews]
+  for review in reviews:
+    r_name = session.execute("select name from students where studentid=:val", {'val': review['reviewerid']})
+    review['reviewerid'] = r_name.fetchone()[0]
+    p_name = session.execute("select name from students where studentid=:val", {'val': review['partnerid']})
+    review['partnerid'] = p_name.fetchone()[0]
+  
+  
+  return render_template('prof.html', reviews=reviews, prof=id, course=course)
+
 
 @app.route('/professors/<name>', methods = ['GET', 'POST'])
 def get_profs(name):

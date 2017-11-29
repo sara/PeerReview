6d40c4a3ba757bf1aadd97cdd0c939b7de2e0dfd
SaraAnn Stanway <sarastanway@gmail.com>
2017-11-29 15:29:03 -0500

    fixed search form validation...again

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,24 @@ from wtforms import Form, SelectMultipleField, RadioField, StringField, IntegerF
 from models import session, Students
 from flask import render_template
 
+class ReviewForm(Form):
+  classID =  RadioField('Class Code', choices = [('213', '213'),('214', '214'), ('336', '336'), ('344', '344'), ('352', '352'), ('416', '416'), ('440', '440')])
+  reviewerid = IntegerField('Reviewer ID', [validators.InputRequired(message='Looks like you didn\'t put in a reviewer ID')])
+  partnerid = IntegerField('Partner ID', [validators.InputRequired(message='Looks like you didn\'t put in a partner ID')])
+  grade = IntegerField('Grade')
+  percentage = IntegerField('What percentage of work did they contribute?', [validators.InputRequired(message='Maybe your partner wasn\'t great, but you still need to list a work contribution!')])
+  repartner = RadioField('Would you partner again?', choices = [('1', 'Yes'), ('0', 'No')])
+  professorid = RadioField ('Professor Name', [validators.InputRequired(message='You forgot to select a professor')], choices = [('Sesh', 'Sesh'), ('Imielinski', 'Imielinski'), ('Miranda', 'Miranda'), ('Muthukrishnan', 'Muthukrishnan'), ('Martin', 'Martin'), ('Francisco', 'Francisco'), ('Bolaris', 'Bolaris'), ('Santosh', 'Santosh'), ('FarachColton', 'FarachColton'), ('Nath', 'Nath'), ('PK', 'PK'), ('Bekris', 'Bekris')])
+
+  #def validate(form):
+  #  classID = form['classID']
+  #  prof = form['professorid']
+  #  if classID == 213:
+    
+
+
+
+
 class SearchForm(Form):
   studentid = StringField('Student Name')
   professorid = RadioField ('Professor Name', choices = [('Sesh', 'Sesh'), ('Imielinski', 'Imielinski'), ('Miranda', 'Miranda'), ('Muthukrishnan', 'Muthukrishnan'), ('Martin', 'Martin'), ('Francisco', 'Francisco'), ('Bolaris', 'Bolaris'), ('Santosh', 'Santosh'), ('FarachColton', 'FarachColton'), ('Nath', 'Nath'), ('PK', 'PK'), ('Bekris', 'Bekris')])
@@ -12,9 +30,9 @@ class SearchForm(Form):
     student = form.data['studentid']
     prof = form.data['professorid']
     classID = form.data['classID']
-    if prof != 'None' and student != 'None':
+    if prof != 'None' and student != '':
       return False
-    if student != 'None' and classID != 'None':
+    if student != '' and classID != 'None':
       return False
     if prof != 'None' and classID != 'None':
       return False
@@ -28,24 +46,3 @@ class ProfileForm(Form):
   gradyear = RadioField('GradYear', [validators.InputRequired(message='Uh Oh! Looks like you forgot to put in a graduation year.')], choices = [('2018', '2018'), ('2019', '2019'), ('2020', '2020'), ('2021', '2021')])
 
   classID =  SelectMultipleField('Classes Taken - Select all that apply!', choices = [('213', '213'),('214', '214'), ('336', '336'), ('344', '344'), ('352', '352'), ('416', '416'), ('440', '440')])
-  
-  #def validate(form):
-  #  studentid = form.data['studentid']
-  #  name = form.data['name']
-  #  gradyear = form.data['gradyear']
-  #  return render_template('error.html', error = studentid, link="/")
-  #  if not studentid:
-  #    return render_template('error.html', error = 'Did you forget to put in an RUID?', link = "/create")
-  #  else:
-  #    return studentid
-    #raise validators.ValidationError('Did you forget to put in an RUID?')
-  #  if not name:
-  #    return render_template('error.html', error = 'Did you forget to put in a name?', link = "/create")
-    #raise validators.ValidationError('Did you forget to put in a name?')
-  #  elif gradyear == 'None':
-  #    return render_template('error.html', error = 'Did you forget to put in a grad year?', link = "/create")
-    #raise validators.ValidationError('Did you forget to put in a grad year?')
-  #duplicate = session.execute("select studentid from students where studentid=:val", {'val': studentid}).fetchall()
-    #if duplicate:
-    #  raise validators.ValidationError('Looks like that RUID is already registered!')
-  #  return True
